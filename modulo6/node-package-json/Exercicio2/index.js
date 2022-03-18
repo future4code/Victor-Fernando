c1caa397d96e40ad05e7dacb2448f759344b4384
unknown <victorsantos732@gmail.com>
2022-03-17 21:59:41 -0300

    terminei o exercicio de  Node e package.json

---
+++ modulo6/node-package-json/Exercicio2/index.js
@@ -0,0 +1,29 @@
+// 2 
+
+// const primeiro = Number(process.argv[3])
+// const segundo = Number(process.argv[4])
+
+// const soma = primeiro + segundo
+// const sub = primeiro - segundo
+// const mult = primeiro * segundo
+// const div = primeiro / segundo
+
+
+// switch(process.argv[2]){
+// 	case "add":
+// 		console.log("Resposta:", soma)
+// 		break;
+// 	case "sub":
+// 		console.log("Resposta:", sub)
+// 		break;
+//     case "mult":
+// 		console.log("Resposta:", mult)
+// 		break;
+//     case "div":
+//         console.log("Resposta:", div)
+//         break;
+// }
+
+
+
+
